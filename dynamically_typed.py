608ed4a0a38edca2da55d19514e632cf9792000d
Deepak Marathe <deepak.marathe@makemytrip.com>
2017-10-09 00:28:25 +0530

    python is dynamically typed

---
+++ dynamically_typed.py
@@ -0,0 +1,16 @@
+# Variables are pointers 
+# x = 4 means x is a pointer to some memory location which holds the value. 
+# It is not required to declare the variables with type information or even the type of variable it points to can be different. It is called dynamic typing.
+
+# x is an integer
+x = 1
+print("x is : ", x)
+
+# x is a string
+x = "Hello"
+print("x is : ", x)
+
+# x is a list
+x = [1,2,3]
+print("x is : ", x)
+
