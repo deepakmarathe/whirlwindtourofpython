d6899f38913f6b778eb0fdf84ca8ad73a0c4e30b
Deepak Marathe <deepak.marathe@makemytrip.com>
2017-10-09 01:19:32 +0530

    bitwise operator

---
+++ operators/bitwise_operators.py
@@ -0,0 +1,15 @@
+# a & b		bitwise and
+# a | b		bitwise or 
+# a ^ b		bitwise xor
+# a << b	bitwise leftshift
+# a >> b 	bitwise rightshift
+# ~a		bitwise not
+
+# bitwise operators make sense on binary numbers, obtained by 'bin' method
+print "10 in binary : ", bin(10)
+print "4 in binary : ",bin(4)
+
+# find out the number which combines the bits in 4 and 10.
+print "(4 | 10) : ", 4 | 10
+print "(4 | 10) in binary : ", bin(4 | 10)
+
