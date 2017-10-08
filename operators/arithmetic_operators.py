2e4c654e230fd83ded25b5e22b05136964437275
Deepak Marathe <deepak.marathe@makemytrip.com>
2017-10-09 01:19:04 +0530

    arithmetic operators

---
+++ operators/arithmetic_operators.py
@@ -0,0 +1,19 @@
+# a + b		addition
+# a - b		subtraction
+# a * b		multiplication
+# a / b		division
+# a // b	floor division
+# a % b 	modulus
+# a ** b 	exponentiation
+# -a 		negation
+# +a 		unary plus
+
+# addition, subtraction and multiplication
+print (4+8) * (6.5-3)
+
+# true division
+print (11/2)
+
+# floor division
+print (11 // 2)
+
