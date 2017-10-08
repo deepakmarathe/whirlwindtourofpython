e229625a100afe63e8aa9230c5c58b79e3afb4cb
Deepak Marathe <deepak.marathe@makemytrip.com>
2017-10-09 00:55:08 +0530

    attributes and methods on object

--- everything_is_an_object.py
+++ everything_is_an_object.py
@@ -9,3 +9,13 @@ x = 3.14
 print type(x)
 
 # Python has types. However, types are not linked to the variable names, but the object themselves. 
+
+# objects have attributes and methods. 
+x = 4.5
+
+# real and imag are the attributes
+print x.real, "+", x.imag, "i"
+
+# is_integer() is a method.
+print x.is_integer()
+
