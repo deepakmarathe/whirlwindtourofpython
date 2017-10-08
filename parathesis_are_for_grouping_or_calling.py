5fdfeff9aac4c959666909b8a2aa8725024130f8
Deepak Marathe <deepak.marathe@makemytrip.com>
2017-10-09 00:28:58 +0530

    paranthesis are for grouping or function call

---
+++ parathesis_are_for_grouping_or_calling.py
@@ -0,0 +1,12 @@
+# paranthesis are used for grouping in an expression
+x = 2 + (3 * 4)
+print("2 + (3 * 4) is : ", x)
+
+# paranthesis are used for function call with parameters
+print("first value is ", 1)
+
+# paranthesis are used for function call 
+l = [4,2,3,1]
+l.sort()
+print(l)
+
