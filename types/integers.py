f182d4b1500fca105cdd730f0aae9199b988d648
Deepak Marathe <deepak.marathe@makemytrip.com>
2017-10-09 01:52:34 +0530

    simple types - integer

---
+++ types/integers.py
@@ -0,0 +1,13 @@
+# Integers
+
+x = 1
+print type(x)
+
+# python integers are variabe precision 
+x = 2 ** 200 
+print x
+
+# division upcasts to floating point type [feature of python 3. python 2 truncates any decimal and return integer
+print 5/2
+
+
