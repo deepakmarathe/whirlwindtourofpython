81616d112d7f90c04474c12a1ef25c964158743a
Deepak Marathe <deepak.marathe@makemytrip.com>
2017-10-09 01:28:39 +0530

    comparision operators

---
+++ operators/comparison_operators.py
@@ -0,0 +1,22 @@
+# comparison operators return true or false values
+
+# a == b 	a equal to b
+# a != b 	a not equal to b 
+# a < b 	a less than b
+# a > b 	a greater than b
+# a <= b	a less than or equal to b
+# a >= b	a greater than or equal to b
+
+print "25 is odd : ", 25 % 2 == 1
+
+print "66 is odd : ", 66 % 2 == 1
+
+# check if a is between 15 and 30
+a = 25
+is_a_between_15_and_30  = 15 < a < 30
+print is_a_between_15_and_30
+
+# 2's complement representation. flipping all the bits of 0 turns the number into -1.
+print -1 == ~0
+
+
