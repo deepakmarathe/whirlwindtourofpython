d80baf8f0008244ee33562e00eda4ea491701ff7
Deepak Marathe <deepak.marathe@makemytrip.com>
2017-10-09 00:47:57 +0530

    everything is an object

---
+++ everything_is_an_object.py
@@ -0,0 +1,11 @@
+# Everything is an object. 
+x = 4
+print type(x)
+
+x = 'hello'
+print type(x)
+
+x = 3.14
+print type(x)
+
+# Python has types. However, types are not linked to the variable names, but the object themselves. 
