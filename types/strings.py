52a0a12f38b609c5019e58a8968cc0c0ac012059
Deepak Marathe <deepak.marathe@makemytrip.com>
2017-10-09 11:17:23 +0530

    strings

---
+++ types/strings.py
@@ -0,0 +1,24 @@
+# Strings
+
+# strings are created with single or double quotes
+message = "what do you like?"
+response = 'spam'
+
+# length of string
+print "len(message) : ", len(message)
+
+# make uppercase
+print "response.upper() : ", response.upper()
+
+# capitalize
+print "message.capitalize() : ", message.capitalize()
+
+# concatenation with plus
+print "message + response : ", message + response
+
+# multiplication is multiple concatenation 
+print "5 * response : ", 5 * response
+
+# zero based indexing
+print "message[0] : ", message[0]
+
