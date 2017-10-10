504bbcd2866d1aa220508125ad52c8a11bc86dde
Deepak Marathe <deepak.marathe@makemytrip.com>
2017-10-10 19:45:23 +0530

    filter as an iterator

--- iterators/map_filter_iterator.py
+++ iterators/map_filter_iterator.py
@@ -2,6 +2,12 @@
 
 # Map
 square = lambda x : x ** 2
+print "-- squares --"
 for i in map(square, range(10)) :  
+	print i
+
+print "-- even numbers --"
+is_even = lambda x : x % 2 == 0
+for i in filter(is_even, range(10)):
 	print i
 
