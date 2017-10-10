8fb67bb2e246e995d8ab2cf380e18c3952ee87d0
Deepak Marathe <deepak.marathe@makemytrip.com>
2017-10-10 19:41:35 +0530

    iter and range iterators

---
+++ iterators/iter.py
@@ -0,0 +1,19 @@
+# iter 
+
+I = iter([1, 2, 3])
+print "next(I) : ", next(I)
+print "next(I) : ", next(I)
+print "next(I) : ", next(I)
+try:
+	print "next(I) : ", next(I)
+except :  
+	print "Stop iteration"
+
+print "----- begin -----"
+# N = 10 ** 12
+# for i in range(N):
+#	if i >= 10 : break
+#	print i
+
+	
+
---
+++ iterators/range_iterator.py
@@ -0,0 +1,12 @@
+# Range iterator
+
+print "---- begin -----"
+for i in range(10):
+	print i
+print "----- end -----"
+
+print "---- begin ----"
+for i in [2, 4, 6, 8, 10]:
+	print i+1
+print "---- end ----"
+
