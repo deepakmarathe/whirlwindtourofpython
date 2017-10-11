d8c0b6ba1c480e1b663921080d7681fdea248934
Deepak Marathe <deepak.marathe@makemytrip.com>
2017-10-11 23:28:35 +0530

    numpy primer

--- data_science_tools/numpy_package.py
+++ data_science_tools/numpy_package.py
@@ -1,8 +1,19 @@
 # Numpy : Numerical Python
 
-import numpy
+import numpy as np
 
-x = numpy.arange(1,10)
+x = np.arange(1,10)
 print x
 
+print x ** 2
+print [i ** 2 for i in range(1, 10)]
 
+print x.reshape((3,3))
+print x.reshape((3,3)).T
+
+print np.dot(x.reshape(3,3), [5, 6, 7])
+
+print np.linalg.eigvals(x.reshape(3,3))
+
+M = x.reshape((3,3))
+print "M : ", M
