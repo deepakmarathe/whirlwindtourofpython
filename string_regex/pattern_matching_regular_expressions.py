04ea4bee6c6e6096ae5ae364d912ffb525cffcb6
Deepak Marathe <deepak.marathe@makemytrip.com>
2017-10-11 12:40:16 +0530

    some more regex with email

--- string_regex/pattern_matching_regular_expressions.py
+++ string_regex/pattern_matching_regular_expressions.py
@@ -25,3 +25,11 @@ print regex.sub('BEAR', line)
 # the original line is not changed.
 print line
 
+email = re.compile('\w+@\w+\.[a-z]{3}')
+text = "To email Guido, try guido@python.org or the older address guido@google.com"
+print email.findall(text)
+print email.sub('--@--.--', text)
+
+# Regular expressions can sometimes be unforgiving, that it drops part of the email address because of the dot
+print email.findall('barack.obama@whitehouse.gov')
+
