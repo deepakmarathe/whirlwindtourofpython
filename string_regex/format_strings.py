6c09f356ed93312f9b98f9670ee42bb205aee10c
Deepak Marathe <deepak.marathe@makemytrip.com>
2017-10-11 11:08:39 +0530

    string formatting operations

---
+++ string_regex/format_strings.py
@@ -0,0 +1,15 @@
+# Formatting the strings
+
+pi = 3.14159
+print "the value of pi is : ", str(pi)
+
+print "the value of pi is {}".format(pi)
+
+print """first name {0}, last name {1}""".format('Richard', 'Thaler')
+
+print """first {first}, last {last}""".format(last='Kahneman', first = 'daniel')
+
+print "{0:.3f}".format(pi)
+
+
+
