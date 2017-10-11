dfa9961456979929513bff63be6557e7e88c1c9a
Deepak Marathe <deepak.marathe@makemytrip.com>
2017-10-11 11:08:21 +0530

    find replace and string manipulation

--- string_regex/find_replace.py
+++ string_regex/find_replace.py
@@ -14,3 +14,27 @@ else :
 	print "done"
 finally : 
 	print "cleaning up"
+
+print line.rfind('a')
+print line.endswith('dog')
+print line.startswith('fox')
+
+print line.replace('brown', 'red')
+print line
+print line.replace('o','--')
+
+print line.partition('fox')
+print line.split()
+
+haiku = """matsushima-ya
+ aah matsushima-ya
+ matsushima-ya"""
+splitlines = haiku.splitlines()
+print splitlines
+
+print "--".join(['1', '2', '3'])
+
+print "\n".join(splitlines)
+
+
+
